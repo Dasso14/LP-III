bcf1cd00b4bb96e685496bf9f2e46ce278de37dc
Julio Centeno <jjulioccenteno@gmail.com>
2023-08-19 20:48:33 -0500

    Segundo cambio

---
+++ Main_2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int vida, rp;
+        boolean fl = false;
+        final int NUMERO_SECRETO = 1234;
+        vida = 0;
+        Scanner sc = new Scanner(System.in);
+        
+        do {
+            vida++;
+            System.out.println("Vida: " + vida + "\t¿Cuál es el número secreto? ");
+            
+            while (!sc.hasNextInt()) {
+                System.out.println("Entrada inválida. Introduce un número válido: ");
+                sc.next(); 
+            }
+            
+            rp = sc.nextInt();
+            if (rp == NUMERO_SECRETO) {
+                fl = true;
+                break; 
+            }
+        } while (vida < 3);
+        
+        sc.close();
+        
+        if (fl) {
+            System.out.println("¡Adivinaste!");
+        } else {
+            System.out.println("Ups, perdiste...");
+        }
+        
+        if (!fl && vida == 3) {
+            System.out.println("Intentos agotados. El número secreto era: " + NUMERO_SECRETO);
+        }
+    }
+}

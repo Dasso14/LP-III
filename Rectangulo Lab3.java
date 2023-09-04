f26d75a36f1ab9bac1926e0883a06e013a055afe
luisfabellid0 <132019717+luisfabellid0@users.noreply.github.com>
2023-09-03 23:10:24 -0500

    Update and rename Lab03 - Proyecto 1 to Rectangulo Lab3.java

---
+++ Rectangulo Lab3.java
@@ -0,0 +1,54 @@
+public class Rectangulo {
+	private Coordenada esquina1;
+    private Coordenada esquina2;
+    private String color;
+
+    private static String[] colores = {"verde", "amarillo", "rojo"};
+    private static int ultimoColorIndex = -1;
+
+    public Rectangulo(Coordenada c1, Coordenada c2) {
+        this.esquina1 = c1;
+        this.esquina2 = c2;
+        asignarSiguienteColor();
+    }
+
+    public Rectangulo(int base, int altura) {
+        this.esquina1 = new Coordenada(0, 0);
+        this.esquina2 = new Coordenada(base, altura);
+        asignarSiguienteColor();
+    }
+
+    private void asignarSiguienteColor() {
+        ultimoColorIndex = (ultimoColorIndex + 1) % colores.length;
+        color = colores[ultimoColorIndex];
+    }
+
+    public Coordenada getEsquina1() {
+        return esquina1;
+    }
+
+    public void setEsquina1(Coordenada c) {
+        esquina1 = c;
+    }
+
+    public Coordenada getEsquina2() {
+        return esquina2;
+    }
+
+    public void setEsquina2(Coordenada c) {
+        esquina2 = c;
+    }
+        
+    public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+    public String toString() {
+        return "Rectangulo [Esquina1: " + esquina1 + ", Esquina2: " + esquina2 + "]";
+    }
+}

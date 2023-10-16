a871a74ac45b24cd6e403cff30f018f0f60a5966
luisfabellid0 <132019717+luisfabellid0@users.noreply.github.com>
2023-10-16 09:20:51 -0600

    Create Ejercicio 1 Clase Figura.java

---
+++ Laboratorio 6 Fase 2/Ejercicio 1 Clase Figura.java
@@ -0,0 +1,11 @@
+abstract class Figura {
+    private String nombre;
+
+    public Figura(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+}

448c03e2e2e3fa4530c9e8f09dd712e62fdfe10c
luisfabellid0 <132019717+luisfabellid0@users.noreply.github.com>
2023-10-16 09:20:36 -0600

    Create Ejercicio 1 Clase FiguraBidimensional.java

---
+++ Laboratorio 6 Fase 2/Ejercicio 1 Clase FiguraBidimensional.java
@@ -0,0 +1,9 @@
+class FiguraBidimensional extends Figura {
+    public FiguraBidimensional(String nombre) {
+        super(nombre);
+    }
+
+    public double obtenerArea() {
+        return 0.0;
+    }
+}

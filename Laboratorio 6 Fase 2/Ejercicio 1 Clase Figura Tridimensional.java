c7abf9282d30803bbbc5364b9d677301f176069b
luisfabellid0 <132019717+luisfabellid0@users.noreply.github.com>
2023-10-16 09:18:37 -0600

    Create Ejercicio 1 Clase Figura Tridimensional.java

---
+++ Laboratorio 6 Fase 2/Ejercicio 1 Clase Figura Tridimensional.java
@@ -0,0 +1,13 @@
+class FiguraTridimensional extends Figura {
+    public FiguraTridimensional(String nombre) {
+        super(nombre);
+    }
+
+    public double obtenerVolumen() {
+        return 0.0;
+    }
+
+    public double obtenerArea() {
+        return 0.0;
+    }
+}

499c94c620a612782062f0ce6f24c7448c240d5d
luisfabellid0 <132019717+luisfabellid0@users.noreply.github.com>
2023-10-28 23:50:18 -0500

    Create JList-MarcoLista.java

---
+++ Laboratorio 8  Fase 2/JList-MarcoLista.java
@@ -0,0 +1,45 @@
+import java.awt.FlowLayout;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.ListSelectionModel;
+import javax.swing.JOptionPane;
+
+public class MarcoLista extends JFrame {
+    private final JList<String> listaJListColores;
+    private static final String[] nombresColores = {
+        "Negro", "Azul", "Cyan", "Gris oscuro", "Gris", "Verde",
+        "Gris claro", "Magenta", "Naranja", "Rosa", "Rojo", "Blanco", "Amarillo"
+    };
+    private static final Color[] colores = {
+        Color.BLACK, Color.BLUE, Color.CYAN, Color.DARK_GRAY, Color.GRAY,
+        Color.GREEN, Color.LIGHT_GRAY, Color.MAGENTA, Color.ORANGE,
+        Color.PINK, Color.RED, Color.WHITE, Color.YELLOW
+    };
+
+    public MarcoLista() {
+        super("Prueba de JList");
+        setLayout(new FlowLayout());
+
+        listaJListColores = new JList<String>(nombresColores);
+        listaJListColores.setVisibleRowCount(5);
+        listaJListColores.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        add(new JScrollPane(listaJListColores));
+
+        listaJListColores.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent evento) {
+                getContentPane().setBackground(colores[listaJListColores.getSelectedIndex()]);
+            }
+        });
+
+        // Cambiar el nombre de la ventana
+        String nuevoNombre = JOptionPane.showInputDialog("Nuevo nombre de la ventana:");
+        if (nuevoNombre != null) {
+            setTitle(nuevoNombre);
+        }
+    }
+}
